f2062befb241154607d031293ac2a5707173e1a6
fabianopavan <fabianopavan84@gmail.com>
2018-01-23 14:55:55 +0000

    Aggiunto config.yml e gestito da initializer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,10 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery prepend: true
     rescue_from CanCan::AccessDenied do |exception|
-        debugger
         redirect_to main_app.root_path, alert: exception.message
     end
     
     def index
-        debugger
     
     end
     
