c78f254db82d39defef3133879886cb0b6f5d61b
Irene Giberna <irene.giberna@smacloud.it>
2018-02-02 13:03:25 +0100

    aggiunte tutte le librerie js per minton tramite yarn (a parte modernizr e detect che hanno errori da risolvere)

--- config/webpack/test.js
+++ config/webpack/test.js
@@ -1,3 +1,5 @@
 const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
+// module.exports.node = { fs: 'empty' }
+module.exports.target = "node" // evita problema can't resolve fs alla compilazione di modernizr
