e1c55ec0a44281660f48861306580eadf5f5f243
fabianopavan <fabianopavan84@gmail.com>
2018-02-02 13:46:18 +0000

    Incluso bootstrap 3.3.7 versione sass, framework minton

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/dbadmin', as: 'rails_admin'
-  mount AuthHub::Engine => "/auth_hub"#, as: 'auth_hub'
+  mount AuthHub::Engine => "/auth_hub", as: 'auth_hub'
   
   get "/test/:page" => "test#show"
   
