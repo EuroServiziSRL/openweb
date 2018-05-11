06dde9ac73c24ae10742bdeadc7b9ae20d350e43
fabianopavan <fabianopavan84@gmail.com>
2018-05-11 09:12:27 +0000

    Impostato config per dominio e caricato in var da environment

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+#setto le configurazioni dopo che sono stati caricati gli initializers, anche il config.yml
+module Openweb
+  class Application < Rails::Application
+    
+    #Questa configurazione viene aggiornata a livello di config.yml. Se non specificato li lo prende l'applicazione dalla request
+    config.url_dominio = APP_CONFIG['url_dominio'].blank? ? nil : APP_CONFIG['url_dominio'] 
+  end
+end
