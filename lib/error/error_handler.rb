f56593e8ead2223acc265efc7f7652d0a49a36a2
fabianopavan <fabianopavan84@gmail.com>
2018-11-08 14:04:18 +0000

    Cambiato db di default

--- lib/error/error_handler.rb
+++ lib/error/error_handler.rb
@@ -17,7 +17,7 @@ module Error
     
     #se non riesce a fare la connessione al db che non esiste vado qui, rimetto la connessione sul db di auth_hub e torno in admin
     def no_database_error(exc)
-      Thread.current[:db_name] = 'openweb_'+(Rails.env == 'production' ? 'prod' : 'devel') 
+      Thread.current[:db_name] = 'soluzionipa_new' 
       AuthHub::SpiderModel.establish_connection({})
       flash[:error] = "Non è possibile gestire le configurazioni dell'ente!"
       redirect_to index_admin_path
