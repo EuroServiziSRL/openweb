318c54941cc69f9d89e4b15a7e4ce8da0128edd6
fabiano <fabiano.pavan@soluzionipa.it>
2020-07-10 09:45:05 +0200

    Commentato invio da scheduler per test

--- config/environment.rb
+++ config/environment.rb
@@ -10,5 +10,16 @@ module Openweb
     
     #Questa configurazione viene aggiornata a livello di config.yml. Se non specificato li lo prende l'applicazione dalla request
     config.url_dominio = Settings.url_dominio.blank? ? nil : Settings.url_dominio 
+    
+    # #chiamo lo scheduler per inviare i metadati ad agid
+    # begin
+    #   #AuthHub::InviaMetadataJob.perform_later 'val'
+    #   AuthHub::InviaMetadataJob.set(wait: 2.minutes).perform_later('val')
+    # rescue => exc
+    #   logger.error exc.message
+    #   logger.error exc.backtrace.join("\n")
+    # end
+    
+    
   end
 end
